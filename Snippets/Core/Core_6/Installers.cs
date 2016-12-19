06441eeb378d18e6548aed15aa7e34bce547142a
danielmarbach <daniel.marbach@openplace.net>
2016-12-19 16:15:17 +0100

    Quick spike to show a direction

--- Snippets/Core/Core_6/Installers.cs
+++ Snippets/Core/Core_6/Installers.cs
@@ -1,7 +1,11 @@
 ﻿namespace Core6
 {
+    using System.Collections.Generic;
+    using System.DirectoryServices.AccountManagement;
+    using System.Linq;
     using System.Threading.Tasks;
     using NServiceBus;
+    using NServiceBus.Support;
 
     class ForInstallationOnReplacement
     {
@@ -17,6 +21,65 @@
 
             #endregion
         }
+    }
 
+    class SwitchInstallersWithCommandline
+    {
+        static EndpointConfiguration endpointConfiguration = new EndpointConfiguration("someEndpoint");
+
+        #region InstallersRunWhenNecessaryCommandLine
+
+        public static void Main(string[] args)
+        {
+            var runInstallers = args.Length == 1 && args[0] == "/runInstallers";
+
+            if (runInstallers)
+            {
+                endpointConfiguration.EnableInstallers();
+            }
+        }
+
+        #endregion
+    }
+
+    class SwitchInstallersByMachineNameConvention
+    {
+        async Task Simple(EndpointConfiguration endpointConfiguration)
+        {
+            #region InstallersRunWhenNecessaryMachineNameConvention
+
+            if (!RuntimeEnvironment.MachineName.EndsWith("-PROD"))
+            {
+                endpointConfiguration.EnableInstallers();
+            }
+
+            #endregion
+        }
+    }
+
+    class SwitchInstallersByADMemberShip
+    {
+        async Task Simple(EndpointConfiguration endpointConfiguration)
+        {
+            #region InstallersRunWhenNecessaryADMembership
+
+            if (GetGroupNames("MyUser", "MyDomain").All(u => u != "ProdGroup"))
+            {
+                endpointConfiguration.EnableInstallers();
+            }
+
+            #endregion
+        }
+
+        #region InstallersRunWhenNecessaryADMembershipQuery
+        static List<string> GetGroupNames(string userName, string domain)
+        {
+            var pc = new PrincipalContext(ContextType.Domain, domain);
+            var src = UserPrincipal.FindByIdentity(pc, userName).GetGroups(pc);
+            var result = new List<string>();
+            src.ToList().ForEach(sr => result.Add(sr.UserPrincipalName));
+            return result;
+        }
+        #endregion
     }
 }
